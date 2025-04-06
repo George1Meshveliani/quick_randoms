8ae51d3f2129967086b44485668d444f5eed9617
George Meshveliani <giorgi.meshveliani@gmail.com>
2025-04-07 01:30:18 +0400

    correct error.

--- BinarySearch.c
+++ BinarySearch.c
@@ -11,20 +11,20 @@ int main() {
 }
 
 int binarySearch(int arr[], size_t size, int target) {
-   int left =  0;
-   int right = size - 1;
-   
-   while (left <= right) {
-       int mid = left + (right - left) / 2;
-       if (mid == left) {
-           return mid;
-       } 
-       else if (arr[mid] < target) {
-           left = mid + 1;
-       }
-       else {
-           right = mid - 1;
-       }
-   }
-   return -1; 
+    int left = 0;
+    int right = size - 1;
+    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (arr[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    return -1;
 } 
