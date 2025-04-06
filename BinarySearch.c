3a9eb808c439d2e958ef43e7693e784717eebde9
George Meshveliani <giorgi.meshveliani@gmail.com>
2025-04-07 01:23:18 +0400

    optimal version

--- BinarySearch.c
+++ BinarySearch.c
@@ -6,25 +6,25 @@ int main() {
     int arr[] = {1, 2, 3};
     int size = sizeof(arr) / sizeof(arr[0]);
     
-    printf("%d", binarySearch(arr, size, 5));
+    printf("%d", binarySearch(arr, size, 1));
     return 0;
 }
 
 int binarySearch(int arr[], size_t size, int target) {
-    int left = 0;
-    int right = size - 1;
-
-    while(left <= right) {
-        int mid = (left + right) / 2;
-        if (arr[mid] == target) {
-            return mid;
-        }
-        else if (arr[mid] < target) {
-            left = mid + 1;
-        }
-        else {
-            right = mid - 1;
-        }
-    }
-    return -1;
+   int left =  0;
+   int right = size - 1;
+   
+   while (left <= right) {
+       int mid = left + (right - left) / 2;
+       if (mid == left) {
+           return mid;
+       } 
+       else if (arr[mid] < target) {
+           left = mid + 1;
+       }
+       else {
+           right = mid - 1;
+       }
+   }
+   return -1; 
 } 
