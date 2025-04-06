4fd67c4fa4c718069a62186207f1e97deb36f00d
George Meshveliani <giorgi.meshveliani@gmail.com>
2025-04-07 00:22:24 +0400

    Update BinarySearch.c

--- BinarySearch.c
+++ BinarySearch.c
@@ -1,1 +1,30 @@
-// Binary 
+#include <stdio.h>
+
+int binarySearch(int arr[], size_t size, int target);
+
+int main() {
+    int arr[] = {1, 2, 3};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    printf("%d", binarySearch(arr, size, 5));
+    return 0;
+}
+
+int binarySearch(int arr[], size_t size, int target) {
+    int left = 0;
+    int right = size - 1;
+
+    while(left <= right) {
+        int mid = (left + right) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (arr[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+} 
