97d3660eb031d407291b7d23673b24cbf367c4b8
george1meshveliani <giorgi.meshveliani@gmail.com>
2025-04-11 18:28:58 +0400

    update code more advanced and clear

--- PrefixSum.c
+++ PrefixSum.c
@@ -10,12 +10,14 @@ int main() {
 }
 
 int PrefixSum(int arr[], int i, int j, size_t size) {
-    int P[size];
+    int P[size + 1];
+    P[0] = 0;
+
     if (size > 0) {
-        for (int k = 0; k < size; k++) {
-            P[k] = P[k - 1] + arr[k];
+        for (int k = 1; k <= size; k++) {
+            P[k] = P[k - 1] + arr[k - 1];
         }
     }
 
-    return P[j] - P[i - 1];
+    return P[j + 1] - P[i];
 }
