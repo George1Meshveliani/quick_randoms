54f727c1dfc62333e86d1694942c5131d6ff09fc
george1meshveliani <giorgi.meshveliani@gmail.com>
2025-04-17 14:26:58 +0400

    number of all possible subarrays|

--- Example.cpp
+++ Example.cpp
@@ -2,14 +2,21 @@
 #include <vector>
 using namespace std;
 
-int returnSubArraySize(vector<int> arr);
+int returnArraySize(vector<int> arr);
+int numberOfSubArrays(vector<int> arr);
 
 int main() {
     vector<int> arr = {1, 2, 3};
-    cout << returnSubArraySize(arr) << endl;
+    cout << returnArraySize(arr) << endl;
+    cout << numberOfSubArrays(arr) << endl;
     return 0;
 }
 
-int returnSubArraySize(vector<int> arr) {
+int returnArraySize(vector<int> arr) {
     return arr.size();
+}
+
+int numberOfSubArrays(vector<int> arr) {
+    int n = arr.size();
+    return n * (n + 1) / 2;
 }
