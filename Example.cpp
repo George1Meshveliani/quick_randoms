d0fbb6508b24d585baf751ca3a389f4d6d363837
george1meshveliani <giorgi.meshveliani@gmail.com>
2025-04-17 15:04:28 +0400

    subarray example

--- Example.cpp
+++ Example.cpp
@@ -4,11 +4,21 @@ using namespace std;
 
 int returnArraySize(vector<int> arr);
 int numberOfSubArrays(vector<int> arr);
+vector<int> subArrays(vector<int> arr);
 
 int main() {
     vector<int> arr = {1, 2, 3};
-    cout << returnArraySize(arr) << endl;
-    cout << numberOfSubArrays(arr) << endl;
+    cout << "Array size: " << returnArraySize(arr) << endl;
+    cout << "Number of sub arrays: " << numberOfSubArrays(arr) << endl;
+    
+    
+    for (int i = 0; i < arr.size(); i++) {
+        for (int j = i; j < arr.size(); j++) {
+            for (int k = i; k <= j; k++) {
+                cout << arr[k] << "|";
+            } 
+        }
+    }
     return 0;
 }
 
@@ -20,3 +30,18 @@ int numberOfSubArrays(vector<int> arr) {
     int n = arr.size();
     return n * (n + 1) / 2;
 }
+
+vector<int> subArrays(vector<int> arr) {
+    vector<int> subArr(arr.size(), 0);
+
+    for (int i = 0; i < arr.size(); i++) {
+        for (int j = i; j < arr.size(); j++) {
+            for (int k = i; k <= j; k++) {
+                subArr[j] = arr[i];
+            } 
+        }
+    }
+
+    return subArr;
+}
+
