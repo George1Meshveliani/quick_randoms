585f9be251b61bcb7051f2326739959fb4a119cc
george1meshveliani <giorgi.meshveliani@gmail.com>
2025-04-21 20:30:06 +0400

    update code

--- Example.cpp
+++ Example.cpp
@@ -8,23 +8,37 @@ vector<int> subArrays(vector<int> arr);
 
 int main() {
     vector<int> arr = {1, 2, 3};
+    int n = arr.size();
     cout << "Array size: " << returnArraySize(arr) << endl;
     cout << "Number of sub arrays: " << numberOfSubArrays(arr) << endl;
+
+    for (int start = 0; start < n; start++) {
+        for (int end = start; end < n; end++) {
+            cout << "["; 
+            for (int i = start; i <= end; i++) {
+                cout << arr[i];
+            }
+            cout << "]" << endl;
+        }
+        // cout << "[";
+        // cout << arr[start];
+        // cout << "]" << endl;
+    }
     
 
     
-    for (int i = 0; i < arr.size(); i++) {
-        for (int j = i; j < arr.size(); j++) {
-            cout << "[";
-            for (int k = i; k <= j; k++) {
-                cout << arr[k];
-                if (k != j) {
-                    cout << ",";
-                }
-            } 
-            cout << "]" << endl;
-        }
-    }
+    // for (int i = 0; i < arr.size(); i++) {
+    //     for (int j = i; j < arr.size(); j++) {
+    //         cout << "[";
+    //         for (int k = i; k <= j; k++) {
+    //             cout << arr[k];
+    //             if (k != j) {
+    //                 cout << ",";
+    //             }
+    //         } 
+    //         cout << "]" << endl;
+    //     }
+    // }
     return 0;
 }
 
