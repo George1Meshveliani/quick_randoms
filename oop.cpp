c2f1e37772b8ec13725ac03cf7f95ae0ea3c15a1
george1meshveliani <giorgi.meshveliani@gmail.com>
2025-05-02 16:48:17 +0400

    update code

--- oop.cpp
+++ oop.cpp
@@ -7,16 +7,42 @@ class Car {
     public:
         int year;
         string brand;
+
+        Car(int y, string b) {
+            year = y;
+            brand = b;
+        }
+
+        // Car Validation Method
+        void carValidation() {
+            if (year < 2010) {
+                cout << "Old" << endl;
+            }
+            else {
+                cout << "New" << endl;
+            }
+        }
+
+        int maxSpeed(int speed) {
+            return speed;
+        } 
 };
 
+// void Car::myMethod() {
+//     cout << "2" << endl;
+// };
+
 int main() {
-    Car carObj;
+    
+    Car carObj(1996, "volvo");
+    Car carObj1(2010, "BMW");
 
-    carObj.year = 1996;
-    carObj.brand = "volvo";
+    carObj.carValidation();
+    carObj1.carValidation();
 
-    cout << carObj.year << endl;
-    cout << carObj.brand << endl;
+    cout << "Year: " << carObj.year << endl;
+    cout << "Brand: " << carObj.brand << endl;
+    cout << "Max speed is: " << carObj.maxSpeed(299) << endl;
 
     return 0;
 }
